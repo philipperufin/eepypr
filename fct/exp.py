597f9a6e60cd25194f1900c8d642172058059583
philipperufin <philippe.rufin@googlemail.com>
2022-07-08 15:43:36 +0200

    general update

--- fct/exp.py
+++ fct/exp.py
@@ -2,24 +2,26 @@ import ee
 
 ee.Initialize()
 
-def exportDrive(image, description, folder, scale):
+def exportDrive(image, description, folder, scale, crs='EPSG:4326'):
 
     task = ee.batch.Export.image.toDrive(**{
         'image': image,
         'description': description,
         'folder': folder,
         'scale': scale,
+        'crs': crs,
         'maxPixels': 1e13
     })
     task.start()
 
-def exportRegionDrive(image, description, folder, scale, region):
+def exportRegionDrive(image, description, folder, scale, region, crs='EPSG:4326'):
 
     task = ee.batch.Export.image.toDrive(**{
         'image': image,
         'description': description,
         'folder': folder,
         'scale': scale,
+        'crs': crs,
         'region': region,
         'maxPixels': 1e13
     })
--- fct/txt.py
+++ fct/txt.py
@@ -13,3 +13,12 @@ def TXT(img, bands, radius, ndi=True):
                      .multiply(10000).toInt16() \
                      .rename([bands + '_p50_' + str(radius) + 'ndi']))
     return(img)
+
+
+
+## Get the NIR band.
+## var nir = image.select('N');
+
+## Compute the gray-level co-occurrence matrix (GLCM), get contrast.
+## var glcm = nir.glcmTexture({size: 3}).select('N_contrast').toInt16().rename()
+## var contrast = .select('N_contrast')
